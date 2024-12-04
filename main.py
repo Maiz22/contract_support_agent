75e6342bee8457541cfa06e43a5c9830d3857150
Matthias Schmidt <matthias.schmidt.92@gmx.de>
2024-12-05 00:56:33 +0100

    created user interaction to generate a user and a password

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from user_interaction import get_contract_data_from_user
-from contract import Contract
+from user_interaction import *
+from model import User
+import sys
+
+
+def parse_argvs(args) -> None:
+    new_user = False
+    if args[0] == "new":
+        new_user = True
+    return new_user
 
 
 if __name__ == "__main__":
-    name, creation_date, duration, unit, cost_per_unit = get_contract_data_from_user()
-    contract = Contract(
-        name=name,
-        creation_date=creation_date,
-        duration=duration,
-        duration_unit=unit,
-        cost_per_unit=cost_per_unit,
-    )
-    print(contract)
+    new_user = parse_argvs(sys.argv)
+    last_user = None
+    if new_user or last_user is None:
+        username = enter_username()
+        password1, password2 = enter_password_and_validation_password()
+    user = User(username=username, password1=password1, password2=password2)
+    print(user)
--- user_interaction.py
+++ user_interaction.py
@@ -1,9 +1,31 @@
-def get_contract_data_from_user() -> tuple[str]:
-    name = input("Enter the contract name:\n").strip()
-    creation_date = input("Enter creation date(dd-mm-yyyy):\n").strip()
-    duration_and_unit = input(
-        "Enter contract duration and unit(d|m|y) separated by a space' '.\n"
-    ).strip()
-    duration, unit = duration_and_unit.split()
-    cost_per_unit = input("Enter the cost per duration unit(Just the number): ")
-    return name, creation_date, duration, unit, cost_per_unit
+from getpass import getpass
+
+
+def enter_username() -> str:
+    username = input("Create username: ").strip()
+    if len(username) < 1:
+        print("Invalid username")
+        return enter_username()
+    return username
+
+
+def enter_password_and_validation_password() -> str:
+    pw1 = getpass("Enter password: ").strip()
+    pw2 = getpass("Enter password again: ").strip()
+
+    # frontend validation
+    if len(pw1) < 1 or (pw1 != pw2):
+        print("Passwords empty or did not match!")
+        return enter_password_and_validation_password()
+    return pw1, pw2
+
+
+# def get_contract_data_from_user() -> tuple[str]:
+#    name = input("Enter the contract name:\n").strip()
+#    creation_date = input("Enter creation date(dd-mm-yyyy):\n").strip()
+#    duration_and_unit = input(
+#        "Enter contract duration and unit(d|m|y) separated by a space' '.\n"
+#    ).strip()
+#    duration, unit = duration_and_unit.split()
+#    cost_per_unit = input("Enter the cost per duration unit(Just the number): ")
+#    return name, creation_date, duration, unit, cost_per_unit
